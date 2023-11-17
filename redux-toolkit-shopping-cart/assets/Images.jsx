66821308bf99fa6b55aa2f8ded18f3b27c6e099e
Dimuthu Abeysinghe <41988837+Dimuthu7@users.noreply.github.com>
2023-11-17 10:49:36 +0530

    Add files via upload

---
+++ redux-toolkit-shopping-cart/assets/Images.jsx
@@ -0,0 +1,8 @@
+const Images = {
+    image1 : new URL('./mouse_1.png', import.meta.url).href,
+    image2 : new URL('./mouse_2.png', import.meta.url).href,
+    image3 : new URL('./mouse_3.png', import.meta.url).href,
+    image4 : new URL('./mouse_4.png', import.meta.url).href,
+}
+
+export default Images
